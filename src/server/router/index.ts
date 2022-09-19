8dc4993e86243ffc567a4f9d6d914d73d87304bf
Reese Jones <reesealanj@gmail.com>
2022-09-19 14:14:41 -0500

    remove unused template server router files

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -2,13 +2,8 @@
 import { createRouter } from "./context";
 import superjson from "superjson";
 
-import { exampleRouter } from "./example";
-import { protectedExampleRouter } from "./protected-example-router";
-
 export const appRouter = createRouter()
   .transformer(superjson)
-  .merge("example.", exampleRouter)
-  .merge("auth.", protectedExampleRouter);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
